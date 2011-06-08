f7f943044f1c2abaa20ac0165042cc9081ad04fb
Christian Nau <chrisnau@yahoo.com>
2011-06-08 15:30:09 -0700

    reverted game objects back to singleton

--- src/database/game_objects.rb
+++ src/database/game_objects.rb
@@ -12,17 +12,20 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with Ilium MUD.  If not, see <http://www.gnu.org/licenses/>.
+require 'singleton'
 require 'database/cassandra_dao'
 require 'logging/logging'
 require 'facets/hash/traverse'
 
 # class to interact with "objects" and "object_tags" column families
 class GameObjects
+  include Singleton
   include Logging
+
 
   # gets a record from the 'objects' column family
   # [object_id] id of the object to retrieve
-  def self.get(object_id)
+  def get(object_id)
     log.debug {"getting object #{object_id}"}
     obj = CassandraDao.get(:objects, object_id).to_hash
     
@@ -33,36 +36,68 @@ class GameObjects
     ret
   end
 
+  # gets a record from the 'objects' column family
+  # [object_id] id of the object to retrieve
+  def self.get(object_id)
+    GameObjects.instance.get object_id
+  end
+
   # saves an object to the 'objects' column family
   # [object_id] object id to save
   # [object_hash] object data
-  def self.save(object_id, object_hash)
+  def save(object_id, object_hash)
     log.debug {"saving #{object_id} : #{object_hash}"}
     #convert hash to string keys
     to_insert = object_hash.traverse {|k,v| [k.to_s, v]}
     CassandraDao.insert :objects, object_id, to_insert
   end
 
+  # saves an object to the 'objects' column family
+  # [object_id] object id to save
+  # [object_hash] object data
+  def self.save(object_id, object_hash)
+    GameObjects.instance.save object_id, object_hash
+  end
+
+  # removes an object from the database
+  # [object_id] object id to remove
+  def remove(object_id)
+    log.debug {"removing #{object_id}"}
+    CassandraDao.remove :objects, object_id
+  end
+
   # removes an object from the database
   # [object_id] object id to remove
   def self.remove(object_id)
-    log.debug {"removing #{object_id}"}
-    CassandraDao.remove :objects, object_id
+    GameObjects.instance.remove object_id
   end
 
   # retrieves an object tag for a given tag name and value
   # [tag_name] object tag name to retrieve
   # [val] specific tag to get
-  def self.get_tag(tag_name, val)
+  def get_tag(tag_name, val)
     log.debug {"getting object tag #{tag_name} for #{val}"}
     tag = CassandraDao.get :object_tags, tag_name, val
     log.debug {"retrieved #{tag}"}
     tag
   end
 
+  # retrieves an object tag for a given tag name and value
+  # [tag_name] object tag name to retrieve
+  # [val] specific tag to get
+  def self.get_tag(tag_name, val)
+    GameObjects.instance.get_tag tag_name, val
+  end
+
   # retrieves all object tags for a given tag name
   # [tag_name] object tag name to retrieve
   def self.get_tags(tag_name)
+    GameObjects.instance.get_tags tag_name
+  end
+
+  # retrieves all object tags for a given tag name
+  # [tag_name] object tag name to retrieve
+  def get_tags(tag_name)
     log.debug {"getting tags for #{tag_name}"}
     tags = CassandraDao.get :object_tags, tag_name
     log.debug {"retrieved #{tags}"}
@@ -72,9 +107,25 @@ class GameObjects
   # removes an object tag for a given tag name and value
   # [tag_name] object tag name to remove
   # [val] specific tag to remove
-  def self.remove_tag(tag_name, val)
+  def remove_tag(tag_name, val)
     log.debug {"removing object tag for #{tag_name} for #{val}"}
     CassandraDao.remove :object_tags, tag_name, val
+  end
+
+  # removes an object tag for a given tag name and value
+  # [tag_name] object tag name to remove
+  # [val] specific tag to remove
+  def self.remove_tag(tag_name, val)
+    GameObjects.instance.remove_tag tag_name, val
+  end
+
+  # adds an object tag for a given tag name and value
+  # [tag_name] object tag name to add
+  # [val] specific tag to add
+  # [row_hash] row hash to add to tag
+  def add_tag(tag_name, val, row_hash)
+    log.debug {"adding object tag for #{tag_name} for #{val} : #{row_hash}"}
+    CassandraDao.insert :object_tags, tag_name, {val => row_hash}
   end
 
   # adds an object tag for a given tag name and value
@@ -82,7 +133,6 @@ class GameObjects
   # [val] specific tag to add
   # [row_hash] row hash to add to tag
   def self.add_tag(tag_name, val, row_hash)
-    log.debug {"adding object tag for #{tag_name} for #{val} : #{row_hash}"}
-    CassandraDao.insert :object_tags, tag_name, {val => row_hash}
+    GameObjects.instance.add_tag tag_name, val, row_hash
   end
 end
