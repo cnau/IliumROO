9ad5d9312456c16f65e06cb78641a74a8b94e2cb
Christian Nau <chrisnau@yahoo.com>
2011-06-08 15:31:31 -0700

    changed system logging back to singleton

--- src/database/system_logging.rb
+++ src/database/system_logging.rb
@@ -12,6 +12,7 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with Ilium MUD.  If not, see <http://www.gnu.org/licenses/>.
+require 'singleton'
 require 'database/cassandra_dao'
 require 'logging/logging'
 require 'simple_uuid'
@@ -19,6 +20,7 @@ require 'simple_uuid'
 # This class wraps interactions with the 'log' column family
 class SystemLogging
   include SimpleUUID
+  include Singleton
   include Logging
 
   # adds a log entry
@@ -29,7 +31,18 @@ class SystemLogging
   # object_id is used in the case of log messages like this: "Foo gave the sword to Bar"
   # where "Foo" is source_id, "Bar" is target_id, and "sword" is object_id
   def self.add_log_entry(msg, source_id = nil, target_id = nil, object_id = nil)
-    log.debug {"adding log entry:#{msg}, source:#{source_id}, target:#{target_id}, object:#{object_id}"}
+    SystemLogging.instance.add_log_entry msg, source_id, target_id, object_id
+  end
+  
+  # adds a log entry
+  # [msg] log message to add
+  # [source_id] optional. source object id reference to objects column family
+  # [target_id] optional. target object id reference to objects column family
+  # [object_id] optional. object object id reference to objects column family
+  # object_id is used in the case of log messages like this: "Foo gave the sword to Bar"
+  # where "Foo" is source_id, "Bar" is target_id, and "sword" is object_id
+  def add_log_entry(msg, source_id = nil, target_id = nil, object_id = nil)
+    log_debug "adding log entry:#{msg}, source:#{source_id}, target:#{target_id}, object:#{object_id}"
     row_id = UUID.new
     log_row = {}
     log_row['log_id'] = row_id.to_guid.to_s
@@ -45,7 +58,7 @@ class SystemLogging
     CassandraDao.insert :log, target_id, row_hash unless target_id.nil?
     CassandraDao.insert :log, object_id, row_hash unless object_id.nil?
 
-    log.debug {"add log id #{row_id.to_guid.to_s}"}
+    log_debug "add log id #{row_id.to_guid.to_s}"
     row_id    # return the log id
   end
 
@@ -55,7 +68,16 @@ class SystemLogging
   # [target_id] super column key that contains log id
   # [object_id] super column key that contains log id
   def self.remove_log_entry(log_id, source_id = nil, target_id = nil, object_id = nil)
-    log.debug {"removing log entry:#{log_id.to_guid.to_s}, source:#{source_id}, target:#{target_id}, object:#{object_id}"}
+    SystemLogging.instance.remove_log_entry log_id, source_id, target_id, object_id
+  end
+
+  # removes a log entry.  log entries in 'all' super column key are always removed
+  # [log_id] a UUID for the log entry to remove
+  # [source_id] super column key that contains log id
+  # [target_id] super column key that contains log id
+  # [object_id] super column key that contains log id
+  def remove_log_entry(log_id, source_id = nil, target_id = nil, object_id = nil)
+    log_debug "removing log entry:#{log_id.to_guid.to_s}, source:#{source_id}, target:#{target_id}, object:#{object_id}"
     CassandraDao.remove :log, 'all', log_id
     CassandraDao.remove :log, source_id, log_id unless source_id.nil?
     CassandraDao.remove :log, target_id, log_id unless target_id.nil?
@@ -68,7 +90,16 @@ class SystemLogging
   # [target_id] super column key that contains log id
   # [object_id] super column key that contains log id
   def self.get_log_entry(log_id, source_id = nil, target_id = nil, object_id = nil)
-    log.debug {"getting log entry:#{log_id.to_guid.to_s}, source:#{source_id}, target:#{target_id}, object:#{object_id}"}
+    SystemLogging.instance.get_log_entry log_id, source_id, target_id, object_id
+  end
+
+  # gets a log entry.  log entries in 'all' super column key are always retrieved
+  # [log_id] a UUID for the log entry to get
+  # [source_id] super column key that contains log id
+  # [target_id] super column key that contains log id
+  # [object_id] super column key that contains log id
+  def get_log_entry(log_id, source_id = nil, target_id = nil, object_id = nil)
+    log_debug "getting log entry:#{log_id.to_guid.to_s}, source:#{source_id}, target:#{target_id}, object:#{object_id}"
     ret = {}
     
     ret['all'] = CassandraDao.get :log, 'all', log_id
@@ -76,7 +107,7 @@ class SystemLogging
     ret[target_id] = CassandraDao.get :log, target_id, log_id unless target_id.nil?
     ret[object_id] = CassandraDao.get :log, object_id, log_id unless object_id.nil?
 
-    log.debug {"retrieved log entries:#{ret}"}
+    log_debug "retrieved log entries:#{ret}"
 
     ret
   end
@@ -85,9 +116,16 @@ class SystemLogging
   # [log_key] default 'all'. super column key to retrieve log entries for
   # [columns_and_options] optional. see http://blog.evanweaver.com/files/doc/fauna/cassandra/classes/Cassandra.html
   def self.get_log(log_key = 'all', *columns_and_options)
-    log.debug {"getting log entries for #{log_key}, #{columns_and_options}"}
+    SystemLogging.instance.get_log log_key, *columns_and_options
+  end
+
+  # retrieves log entries for a give super column key
+  # [log_key] default 'all'. super column key to retrieve log entries for
+  # [columns_and_options] optional. see http://blog.evanweaver.com/files/doc/fauna/cassandra/classes/Cassandra.html
+  def get_log(log_key = 'all', *columns_and_options)
+    log_debug "getting log entries for #{log_key}, #{columns_and_options}"
     entries = CassandraDao.get :log, log_key, *columns_and_options
-    log.debug {"got log entries #{entries}"}
+    log_debug "got log entries #{entries}"
     entries
   end
 
@@ -95,9 +133,16 @@ class SystemLogging
   # [log_key] default 'all'. super column key to retrieve log count for
   # [columns_and_options] optional. see http://blog.evanweaver.com/files/doc/fauna/cassandra/classes/Cassandra.html
   def self.get_log_count(log_key = 'all', *columns_and_options)
-    log.debug {"counting logs for #{log_key}, #{columns_and_options}"}
+    SystemLogging.instance.get_log_count log_key, *columns_and_options
+  end
+
+  # retrieves count of log entries for a give super column key
+  # [log_key] default 'all'. super column key to retrieve log count for
+  # [columns_and_options] optional. see http://blog.evanweaver.com/files/doc/fauna/cassandra/classes/Cassandra.html
+  def get_log_count(log_key = 'all', *columns_and_options)
+    log_debug "counting logs for #{log_key}, #{columns_and_options}"
     count = CassandraDao.count_columns :log, log_key, *columns_and_options
-    log.debug {"found #{count} logs in #{log_key}"}
+    log_debug "found #{count} logs in #{log_key}"
     count
   end
 end
